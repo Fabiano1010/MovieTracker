c612f3b8c04a54e253884ee66c85730eb1da8e92
Fabiano1010 <fabian.skrzy@gmail.com>
2026-01-13 15:17:16 +0100

    Style changes

--- resources/js/Layouts/Layout.vue
+++ resources/js/Layouts/Layout.vue
@@ -12,15 +12,15 @@ import Dashboard from "@/Pages/Dashboard.vue";
 
 
                     <div class="space-x-6">
-                        <Link :href="route('home')" class="nav-link" :class="{'bg-orange-400' : $page.component === 'Home' }">Home</Link>
+                        <Link :href="route('home')" class="nav-link" :class="{'nav-link-current' : $page.component === 'Home' }">Home</Link>
                     </div>
                     <div v-if="$page.props.auth.user" class="space-x-6">
-                        <Link :href="route('dashboard')" class="nav-link" :class="{'bg-orange-400' : $page.component === 'Dashboard' }">Dashboard</Link>
+                        <Link :href="route('dashboard')" class="nav-link" :class="{'nav-link-current' : $page.component === 'Dashboard' }">Dashboard</Link>
                         <Link :href="route('logout')" method="post" as="button" type="button" class="nav-link">Logout</Link>
                     </div>
                     <div class="space-x-6" v-else>
-                        <Link :href="route('register')" class="nav-link" :class="{'bg-orange-400' : $page.component === 'Auth/Register' }">Register</Link>
-                        <Link :href="route('login')" class="nav-link" :class="{'bg-orange-400' : $page.component === 'Auth/Login' }">Login</Link>
+                        <Link :href="route('register')" class="nav-link" :class="{'nav-link-current' : $page.component === 'Auth/Register' }">Register</Link>
+                        <Link :href="route('login')" class="nav-link" :class="{'nav-link-current' : $page.component === 'Auth/Login' }">Login</Link>
                     </div>
 
 
