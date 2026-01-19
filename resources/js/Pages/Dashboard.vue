ca5f5f5f435d15997d8eff1559f0905b5f2615df
Fabiano1010 <fabian.skrzy@gmail.com>
2026-01-19 12:59:34 +0100

    fixed user movies infinite scroll

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,54 +1,64 @@
 <script setup>
-
-import { onMounted, onUnmounted, ref , watch } from 'vue';
-import {router, useForm, usePage} from '@inertiajs/vue3';
+import { onMounted, onUnmounted, ref, watch } from 'vue';
+import { router, useForm, usePage } from '@inertiajs/vue3';
 import UserMovieCard from "@/Components/UserMovieCard.vue";
+
 const page = usePage();
 
 const props = defineProps({
-    movies:{
+    movies: {
         type: Object,
-        default: () => ({ titles:[] })
+        default: () => ({ data: [] })
     },
     error: {
         type: String,
         default: null
     },
-    processing:  true,
+    processing: {
+        type: Boolean,
+        default: true
+    },
 })
 
 const form = useForm({
     selectedStatus: '',
     fav: false,
     sortOption: 'desc'
-
 })
-
 
 const loading = ref(false)
 const currentPage = ref(1)
 const containerRef = ref(null)
 const allMovies = ref([])
 const isProcessing = ref(props.processing)
+
+
+watch(() => props.movies?.data, (newData) => {
+    if (newData && !loading.value) {
+        allMovies.value = newData
+    }
+}, { immediate: true })
+
 const fetchData = (page = 1, append = false, reset = false) => {
     loading.value = true
-    if(reset) currentPage.value = 1
+    if (reset) {
+        currentPage.value = 1
+        allMovies.value = []
+    }
+
     router.get(route('movies.index'), {
         status: form.selectedStatus,
         sort_order: form.sortOption,
         is_favourite: form.fav,
         page: page
-
     }, {
         preserveState: true,
-        preserveScroll: false,
-        onSuccess: (page) => {
+        preserveScroll: true,
+        onSuccess: (response) => {
             if (append) {
-
-                allMovies.value = [...allMovies.value, ...page.props.movies.data]
+                allMovies.value = [...allMovies.value, ...response.props.movies.data]
             } else {
-
-                allMovies.value = page.props.movies.data
+                allMovies.value = response.props.movies.data
             }
             isProcessing.value = false
             loading.value = false
@@ -58,10 +68,8 @@ const fetchData = (page = 1, append = false, reset = false) => {
             loading.value = false
         },
         onFinish: () => {
-            isProcessing.value = false
             loading.value = false
         }
-
     })
 }
 
@@ -69,7 +77,7 @@ const loadMore = () => {
     if (loading.value || !props.movies.next_page_url) return
 
     currentPage.value++
-    fetchData(currentPage.value, true,false)
+    fetchData(currentPage.value, true, false)
 }
 
 const handleScroll = (e) => {
@@ -82,37 +90,32 @@ const handleScroll = (e) => {
 }
 
 onMounted(() => {
-
-    containerRef.value = document.getElementById('userContainer')
-
     if (containerRef.value) {
-
         containerRef.value.addEventListener('scroll', handleScroll)
     }
-    fetchData();
+    fetchData(currentPage.value, true, false)
 })
 
 onUnmounted(() => {
     if (containerRef.value) {
         containerRef.value.removeEventListener('scroll', handleScroll)
-
     }
 })
-
-
-
 </script>
+
 <template>
     <div>
         <div>
-            <h1 class="title" v-if="$page.props.auth.user.username">Welcome back {{ $page.props.auth.user.username }}!</h1>
+            <h1 class="title" v-if="$page.props.auth.user.username">
+                Welcome back {{ $page.props.auth.user.username }}!
+            </h1>
         </div>
         <div>
             <h1 class="subtitle" v-if="$page.props.auth.user.username">Your movies</h1>
         </div>
 
         <div>
-            <form @submit.prevent="fetchData(1,false,true)" class="movieForm">
+            <form @submit.prevent="fetchData(1, false, true)" class="movieForm">
                 <select v-model="form.selectedStatus">
                     <option selected value="">All</option>
                     <option value="to_watch">To watch</option>
@@ -137,34 +140,42 @@ onUnmounted(() => {
             </form>
         </div>
 
-        <div v-if="props.processing" class="loading">
+        <div v-if="isProcessing" class="loading">
             Loading movies...
         </div>
         <div v-if="page.flash.success" class="detailsMessage">
             {{ page.flash.success }}
         </div>
-        <div v-if="page.flash.error" class="detailsMessage error ">
+        <div v-if="page.flash.error" class="detailsMessage error">
             {{ page.flash.error }}
         </div>
-        <div v-if="$page.props.errors" class="detailsMessage error ">
+        <div v-if="$page.props.errors" class="detailsMessage error">
             {{ $page.props.errors.status }}
         </div>
-        <div id="userContainer" class="movieCardContainer userMoviesContainer" v-if="props.movies?.data?.length">
-            <UserMovieCard v-for="movie in allMovies"
-                           :key="movie.movie_id"
-                           :id="movie.movie_id"
-                           :titleOriginal="movie.original_title  || ''"
-                           :titlePrimary="movie.primary_title  || ''"
-                           :date="String(movie.start_year)  || ''"
-                           :img="movie.primary_img || ''"
-                           :movieStatus="movie.status"
-                           :comment="movie.comment"
-                           :userRating="movie.user_rating"
-                           :fav="movie.is_favourite"
+
+        <div
+            v-if="allMovies?.length"
+            id="userContainer"
+            ref="containerRef"
+            class="movieCardContainer userMoviesContainer"
+            @scroll="handleScroll"
+        >
+            <UserMovieCard
+                v-for="movie in allMovies"
+                :key="movie.movie_id"
+                :id="movie.movie_id"
+                :titleOriginal="movie.original_title || ''"
+                :titlePrimary="movie.primary_title || ''"
+                :date="String(movie.start_year) || ''"
+                :img="movie.primary_img || ''"
+                :movieStatus="movie.status"
+                :comment="movie.comment"
+                :userRating="movie.user_rating"
+                :fav="movie.is_favourite"
             />
         </div>
-        <div v-else>
-            <p class="dashboardEmpty"> Add some movies already!</p>
+        <div v-else-if="!isProcessing">
+            <p class="dashboardEmpty">Add some movies already!</p>
         </div>
 
         <div v-if="loading" class="loading" style="text-align: center; padding: 20px;">
@@ -172,7 +183,3 @@ onUnmounted(() => {
         </div>
     </div>
 </template>
-
-<style scoped>
-
-</style>
